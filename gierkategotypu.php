611a7d493e13767099cfe766ec5c8ab3b7a28647
Swiezyyy <45047369+Swiezyyy@users.noreply.github.com>
2022-03-15 15:42:05 +0100

    Create gierkategotypu.php

---
+++ gierkategotypu.php
@@ -0,0 +1,14 @@
+<html>
+<form action:" " method="Post">
+        <select name="Klasa">
+            <option name=silacz> Siłacz </option>
+            <option name=bosniak> Bośniak </option>
+            <option name=biegacz> Biegacz </option>
+            <option name=rusek> Rusek </option>
+        </select><br>
+            Dodaj punkty umiejętności <br>
+            
+
+
+
+</html>
